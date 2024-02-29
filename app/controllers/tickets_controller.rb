8125a729731c24e78eb399220cb6eb6f70988a50
Marina Lima <marinaarrudalima@gmail.com>
2024-02-29 14:26:28 -0300

    ticket routes added

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,22 @@
 class TicketsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def tickets_params
+    params.require(tickets).permit(:artist, :ticket_type, :price, :city, :event_date, :user_id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -34,9 +34,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_29_152015) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.string "phone_number"
+      t.string "first_name"
+      t.string "last_name"
+      t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
