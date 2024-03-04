b59b86b29b8d95a780e0042b31d7a5aa35ee9ccb
Carolina Leone <carol.leone@hotmail.com>
2024-03-04 15:26:58 -0300

    my_tickets_path para orders_path

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,17 @@
 class OrdersController < ApplicationController
+  def index
+    @orders = Order.where(user: current_user)
+  end
+
   def create
     @ticket = Ticket.find(params[:ticket_id])
     @order = Order.new
     @order.user = current_user
     @order.ticket = @ticket
+    @ticket.sold!
 
     if @order.save
-      redirect_to my_tickets_path
+      redirect_to orders_path
     else
       render "tickets/show"
     end
